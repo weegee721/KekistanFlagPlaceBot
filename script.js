3c17b1a2c9b34a8446c374d99b64e2e04c290035
weegee721 <weegee721.hd@gmail.com>
2017-04-03 01:09:13 -0700

    Add files via upload
    f

--- script.js
+++ script.js
@@ -69,7 +69,7 @@
         
         var options = null;
         var syncData = function() {
-            $.getJSON('https://raw.githubusercontent.com/anonymouskek/place/master/sync.json', function(data) {
+            $.getJSON('https://raw.githubusercontent.com/weegee721/KekistanFlagPlaceBot/master/sync.json', function(data) {
                 options = data;
             });  
         };
